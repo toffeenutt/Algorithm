e87a1c68077b597d259b0108c83679feb7dbd699
HAN <tngmlgks99@naver.com>
2021-07-20 21:44:03 +0900

    에라토스테네스의 체

--- math/BJ_15965_K번째 소수.cpp
+++ math/BJ_15965_K번째 소수.cpp
@@ -11,7 +11,7 @@ int main()
 	bool eratosthenes[7368787 + 1] = { 0 };	//eratosthenes[i] == 0 이면 소수
 	//에라토스테네스의 체 이용
 	for (int j = 4; j <= 7368787; j += 2) eratosthenes[j] = 1;    //2의 배수를 미리 합성수로 처리
-	for (int i = 3; i <= sqrt(7368787); ++i)
+	for (int i = 3; i <= sqrt(7368787); i += 2)
 	{
 		if (!eratosthenes[i])	//소수인 경우 그 배수를 모두 합성수로 처리
 		{										//i + i인 이유: 홀수인 j에 홀수인 i를 더하면 짝수가 됨
@@ -19,17 +19,21 @@ int main()
 		}									//짝수인경우를 건너 뛰어서 반복 횟수가 1/2가 됨
 	}
 
-	int count = 1;
-	for (int i = 2; i <= 7368787; ++i)
+	if (N == 1) std::cout << 2;
+	else
 	{
-		if (!eratosthenes[i])
+		int count = 2;
+		for (int i = 3; i <= 7368787; i += 2)
 		{
-			if (N == count)
+			if (!eratosthenes[i])
 			{
-				std::cout << i;
-				break;
+				if (N == count)
+				{
+					std::cout << i;
+					break;
+				}
+				++count;
 			}
-			++count;
 		}
 	}
 }
