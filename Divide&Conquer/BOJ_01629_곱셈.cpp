b0435f2a496f3557ad7bbd65e0998b192d177fbe
HAN <tngmlgks99@naver.com>
2021-09-30 23:18:36 +0900

    분할정복, 거듭제곱

--- Divide&Conquer/BOJ_01629_곱셈.cpp
+++ Divide&Conquer/BOJ_01629_곱셈.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-int ApowBdivideC(int a, int b, int c)
+int ApowBmodC(int a, int b, int c)
 {
 	if (b == 1) return a;
-	if (b % 2) return (long long)a * ApowBdivideC(a, b - 1, c) % c;	//b가 홀수일경우 a * a^(b-1)
-	long long tmp = ApowBdivideC(a, b / 2, c);	//b가 짝수일경우 a^(b/2) * a^(b/2);
+	if (b % 2) return (long long)a * ApowBmodC(a, b - 1, c) % c;	//b가 홀수일경우 a * a^(b-1)
+	long long tmp = ApowBmodC(a, b / 2, c);	//b가 짝수일경우 a^(b/2) * a^(b/2);
 	return tmp * tmp % c;
 }
 int main()
@@ -13,5 +13,5 @@ int main()
 
 	int A, B, C;
 	std::cin >> A >> B >> C;
-	std::cout << ApowBdivideC(A % C, B, C);
+	std::cout << ApowBmodC(A % C, B, C);
 }
