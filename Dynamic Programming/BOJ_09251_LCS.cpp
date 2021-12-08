d330d7c96b2deced34e076c5415036343e11cff1
HAN <tngmlgks99@naver.com>
2021-12-08 21:34:20 +0900

    동적계획법

--- Dynamic Programming/BOJ_09251_LCS.cpp
+++ Dynamic Programming/BOJ_09251_LCS.cpp
@@ -9,9 +9,9 @@ int main()
 	std::ios::sync_with_stdio(false);
 
 	std::cin >> A >> B;
-	//cache[i][j] 가 A[0:i], B[0:j] 둘의 LCS 라고 하자.
+	//cache[i][j] 가 A[0:i], B[0:j] 둘의 LCS의 길이 라고 하자.
 	//A[i] == B[j]이면 A[0:i-1], B[0:j-1]의 LCS의 뒤에 하나를 추가할 수 있으므로 cache[i-1][j-1] + 1 이다.
-	//!= 이면 A[0:i], B[0:j-1] 의 LCS인 cache[i][j-1] 와 A[0:i-1], B[0:j]의 LCS인 cache[i-1][j] 중 더 큰 값과 같다.
+	//!= 이면 A[0:i], B[0:j-1] 의 LCS의 길이인 cache[i][j-1] 와 A[0:i-1], B[0:j]의 LCS의 길이인 cache[i-1][j] 중 더 큰 값과 같다.
 	for (int i = 0; i < A.size(); ++i) for (int j = 0; j < B.size(); ++j)
 	{
 		if (A[i] == B[j]) cache[i][j] = (i == 0 || j == 0 ? 1 : cache[i - 1][j - 1] + 1);
