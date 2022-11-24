a046343645d4fd5e7d0313a98f774e2486e36bdb
HAN <tngmlgks99@naver.com>
2022-11-25 01:55:03 +0900

    DP

--- Dynamic Programming/BOJ_21320_순간이동 여행.cpp
+++ Dynamic Programming/BOJ_21320_순간이동 여행.cpp
@@ -7,23 +7,24 @@ int main()
 	int N, K;
 	std::cin >> N >> K;
 	int cache[3000];
-	cache[0] = 0;
-	for (int i = 1; i < N; ++i)
+	cache[0] = cache[1] = 0;
+	for (int i = 2; i < N; ++i) cache[i] = (cache[i - 1] + (cache[i - 2] << 1) % 1000000007 + 2) % 1000000007;
+
+	if (K == N) std::cout << cache[N - 1];
+	else
 	{
-		cache[i] = 0;
-		int ii = i - 1;
-		for (int j = 0; j < ii; ++j)
+		int ans = 0;
+		int NN = N - 2;
+		for (int i = 0; i < NN; ++i) ans = (ans + 1 + cache[i]) % 1000000007;	//¿À¸¥ÂÊ
+
+		//¿ÞÂÊ
+		if (K == 1) ans = (ans + 1 + cache[NN]) % 1000000007;
+		else if (K == 2) ans = (ans + 2 + (cache[N - 3] << 1) % 1000000007) % 1000000007;
+		else
 		{
-			cache[i] += 2 + (cache[j] << 1);
+			ans = (ans + 2 + (cache[N - 1 - K] << 1) % 1000000007) % 1000000007;
+			for (int i = N - K; i < NN; ++i) ans = (ans + 1 + cache[i]) % 1000000007;
 		}
+		std::cout << ans;
 	}
-	int ans = 0;
-	int NN = N - 2;
-	for (int i = 0; i < NN; ++i) ans += cache[i];
-	ans += (cache[N - (--K)] << 1);
-	while (--K)
-	{
-		ans += cache[K]
-	}
-	std::cout << ans;
 }
